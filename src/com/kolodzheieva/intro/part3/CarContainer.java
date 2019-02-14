a64e8720e1cafbbfd2a46fe0f7df5870b61c4c9c
kolodgeeva <kolodgeeva@gmail.com>
2019-02-14 10:57:07 +0200

    add CarContainer task

---
+++ src/com/kolodzheieva/intro/part3/CarContainer.java
@@ -0,0 +1,24 @@
+package com.kolodzheieva.intro.part3;
+
+public class CarContainer {
+
+    public static double carsCount(double length, double height, double width) {
+        double containerLength = 10;
+        double containerHeight = 4;
+        double containerWidth = 2.5;
+
+        if (length <= containerLength && height <= containerHeight && width <= containerWidth ) {
+            int carsByLength = (int) (containerLength / length);
+            int carsByHeight = (int) (containerHeight / height);
+            int carsByWidth = (int) (containerWidth / width);
+
+            return carsByHeight * carsByLength * carsByWidth;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(carsCount(10, 2, 2.5));
+    }
+
+}
