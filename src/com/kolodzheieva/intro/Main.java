c01a6530ea1fa66103a4efb39c6d19a93f680575
mariana <kolodgeeva@gmail.com>
2019-03-25 09:28:47 +0200

    Add Strings task

--- src/com/kolodzheieva/intro/Main.java
+++ src/com/kolodzheieva/intro/Main.java
@@ -8,41 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //task7();
-        //task8();
-        //task9();
-
-        // Task 12
-        //System.out.println("10 uan to usd = " + Exchanger.uahToUsd(10));
-        //System.out.println("10 usd to uah = " + Exchanger.usdToUah(10));
-
-        // Task 13
-        //System.out.println("Days past for 13.08.56: " + Earth2Calendar.daysPast(1, 1, 3));
-
-        //PathCounter.count(567);
-
-        //System.out.println(EvenOdd.isEven(101));
-
-        //System.out.println(TrainsCollision.isCollisionHappen(0.7, 1));
-
-        //System.out.println(DronsCollision.isCollisionHappen(0, 0, 2, 0, 3, 2));
-        //System.out.println(DronsCollision.isCollisionHappen(1, 1, 1, 4,4, 1));
-
-        //System.out.println(Trajectory.distance(7, 1, -6));
-
-        /*System.out.println("1 - Налево пойдешь, сам умрешь");
-        System.out.println("2 - Направо - коня потеряешь");
-        System.out.println("3 - Прямо - клад найдешь");
-        Scanner scanner = new Scanner(System.in);
-        int point = scanner.nextInt();
-        DirectionSelector.select(point);*/
-
-        /*System.out.println(LoadPlatform.canBeLoaded(100, 100, 1000, 1000));
-        System.out.println(LoadPlatform.canBeLoaded(101, 100, 1000, 1000));
-        System.out.println(LoadPlatform.canBeLoaded(100, 101, 1000, 1000));
-        System.out.println(LoadPlatform.canBeLoaded(100, 100, 1000, 1001));
-        System.out.println(LoadPlatform.canBeLoaded(50, 50, 50, 1000));*/
-
     }
 
 }
---
+++ src/com/kolodzheieva/intro/strings/Annagram.java
@@ -0,0 +1,10 @@
+package com.kolodzheieva.intro.strings;
+
+public class Annagram {
+
+
+    public static void main(String[] args) {
+        String[] strings = {"aaa", "aba", "baa", "ab", "a"};
+
+    }
+}
